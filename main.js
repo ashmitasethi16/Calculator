43e416e3d2c3609c8a6d0b348b2065cc0f0a22d3
Ashmita <ashmitasethi16@gmail.com>
2023-12-20 15:29:53 +0530

    calculator codes

---
+++ main.js
@@ -0,0 +1,27 @@
+var allTd = document.querySelectorAll('#container td');
+ var screen = document.querySelector('#container input')
+
+for(let i =0; i<allTd.length;i++){
+    allTd[i].addEventListener('click',(e)=>{
+        var text = e.target.innerText;
+
+        if(text === '='){
+            try {
+                screen.value = eval(screen.value);
+            } catch (e) {
+                alert('Invalid Operation')
+                screen.value = '';
+            }
+        }else if(text === 'x'){
+            screen.value += '*';
+        }else if(text === 'C'){
+            screen.value ='';
+        }else if(text === 'x2'){
+            screen.value += '**';
+        }
+        else{
+        screen.value += text;
+        }
+    })
+}
+
